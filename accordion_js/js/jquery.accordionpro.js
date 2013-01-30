c2e967f1cfe76deb6fbdd8b182a87e3094b90af6
Nikki <nikki@nicolahibbert.com>
2013-01-30 14:23:56 +0000

    css3 animations, hw acceleration on iOS

--- accordion_js/js/jquery.accordionpro.js
+++ accordion_js/js/jquery.accordionpro.js
@@ -8,6 +8,51 @@
 
 (function(c,b,a,e){var d=function(g,f){this.elem=g;this.$elem=c(g);this.options=f;this.metadata=this.$elem.data("swipe-options")};d.prototype={defaults:{left:function(f){},right:function(f){},up:function(f){},down:function(f){},threshold:{x:100,y:50}},init:function(){this.config=c.extend({},this.defaults,this.options,this.metadata);this.coords={start:{x:0,y:0},end:{x:0,y:0}};c(this.elem).on({touchstart:c.proxy(this.touchStart,this),touchmove:c.proxy(this.touchMove,this),touchend:c.proxy(this.touchEnd,this)});return this},touchEnd:function(f){var g={x:this.coords.start.x-this.coords.end.x,y:this.coords.start.y-this.coords.end.y};if(g.y<this.config.threshold.y&&g.y>(this.config.threshold.y*-1)){if(g.x>this.config.threshold.x){this.config.left()}if(g.x<(this.config.threshold.x*-1)){this.config.right()}}else{if(g.y>=0){this.config.up()}else{this.config.down()}}},touchMove:function(f){f.preventDefault();var g=f.originalEvent.targetTouches[0];this.coords.end={x:g.pageX,y:g.pageY}},touchStart:function(f){var g=f.originalEvent.targetTouches[0];this.coords={start:{x:g.pageX,y:g.pageY},end:{x:g.pageX,y:g.pageY}}}};d.defaults=d.prototype.defaults;c.fn.swipe=function(f){return this.each(function(){new d(this,f).init()})}})(jQuery,window,document);
 
+/* Modernizr 2.6.2 (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-csstransitions-touch-prefixed-teststyles-testprop-testallprops-prefixes-domprefixes
+ */
+
+;window.Modernizr=function(a,b,c){function y(a){i.cssText=a}function z(a,b){return y(l.join(a+";")+(b||""))}function A(a,b){return typeof a===b}function B(a,b){return!!~(""+a).indexOf(b)}function C(a,b){for(var d in a){var e=a[d];if(!B(e,"-")&&i[e]!==c)return b=="pfx"?e:!0}return!1}function D(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:A(f,"function")?f.bind(d||b):f}return!1}function E(a,b,c){var d=a.charAt(0).toUpperCase()+a.slice(1),e=(a+" "+n.join(d+" ")+d).split(" ");return A(b,"string")||A(b,"undefined")?C(e,b):(e=(a+" "+o.join(d+" ")+d).split(" "),D(e,b,c))}var d="2.6.2",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l=" -webkit- -moz- -o- -ms- ".split(" "),m="Webkit Moz O ms",n=m.split(" "),o=m.toLowerCase().split(" "),p={},q={},r={},s=[],t=s.slice,u,v=function(a,c,d,e){var h,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:g+(d+1),l.appendChild(j);return h=["&#173;",'<style id="s',g,'">',a,"</style>"].join(""),l.id=g,(m?l:n).innerHTML+=h,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=f.style.overflow,f.style.overflow="hidden",f.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),f.style.overflow=k),!!i},w={}.hasOwnProperty,x;!A(w,"undefined")&&!A(w.call,"undefined")?x=function(a,b){return w.call(a,b)}:x=function(a,b){return b in a&&A(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=t.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(t.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(t.call(arguments)))};return e}),p.touch=function(){var c;return"ontouchstart"in a||a.DocumentTouch&&b instanceof DocumentTouch?c=!0:v(["@media (",l.join("touch-enabled),("),g,")","{#modernizr{top:9px;position:absolute}}"].join(""),function(a){c=a.offsetTop===9}),c},p.csstransitions=function(){return E("transition")};for(var F in p)x(p,F)&&(u=F.toLowerCase(),e[u]=p[F](),s.push((e[u]?"":"no-")+u));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)x(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof enableClasses!="undefined"&&enableClasses&&(f.className+=" "+(b?"":"no-")+a),e[a]=b}return e},y(""),h=j=null,e._version=d,e._prefixes=l,e._domPrefixes=o,e._cssomPrefixes=n,e.testProp=function(a){return C([a])},e.testAllProps=E,e.testStyles=v,e.prefixed=function(a,b,c){return b?E(a,b,c):E(a,"pfx")},e}(this,this.document);
+
+/**
+ * jQuery animate -> CSS3 Transitions
+ * http://addyosmani.com/blog/css3transitions-jquery/
+ */
+
+$(function() {
+  $.fn.extend({
+    defaultAnimate: $.fn.animate,
+    animate: function(props, speed, easing, callback) {
+      var options = speed && typeof speed === "object" ? jQuery.extend({}, speed) : {
+            complete: callback || !callback && easing ||
+            jQuery.isFunction( speed ) && speed,
+            duration: speed,
+            easing: callback && easing || easing && !jQuery.isFunction(easing) && easing
+          };
+
+      return $(this).each(function() {
+        var $this = $(this),
+            altTransition,
+            easing = (options.easing) ? easing : 'ease-in-out',
+            prefix = (Modernizr.prefixed('transition').replace(/([A-Z])/g, function(str,m1){ return '-' + m1.toLowerCase(); }).replace(/^ms-/,'-ms-'));
+
+            if (Modernizr.csstransitions) {
+              $this.css(prefix, 'all ' + speed / 1000 + 's ease-in-out').css(props);
+              setTimeout(function() {
+                $this.css(prefix, altTransition);
+                if ($.isFunction(options.complete)) {
+                  options.complete();
+                }
+              }, speed);
+            }
+            else {
+              $this.defaultAnimate(props, options);
+            }
+      })
+    }
+  });
+});
+
 /*************************************************!
 *
 *   project:    Accordion Pro - a responsive accordion plugin for jQuery
@@ -35,23 +80,22 @@
 
     defaults = {
       orientation : 'horizontal',             // 'horizontal' or 'vertical' accordion
-      startClosed : false,
-      rtl : false,
+      startClosed : false,                    // start in a closed position
+      rtl : false,                            // right to left layout
 
-      width : '100%',                         // fixed (px/em) or responsive (%)
+      width : '100%',                         // fixed (px/em) or fluid (%)
       height : 320,                           // fixed (px)
       tabSize : 48,                           // fixed (px)
 
-      activateOn : 'click',                   // click or mouseover
+      activateOn : 'click',                   // click or mouseover (tap and swipe touch events enabled by default)
       firstSlide : 1,                         // displays slide (n) on page load
       slideSpeed : 800,                       // slide animation speed
-      onSlideOpen : function(e) {},           // callback on slide activate
-      onSlideClose : function(e) {},          // callback on slide anim complete
+      onSlideOpen : function(e) {},           // callback on slide open
+      onSlideClose : function(e) {},          // callback on slide animation complete
 
       autoPlay : false,                       // automatically cycle through slides
       pauseOnHover : false,                   // pause on hover
       cycleSpeed : 6000,                      // time between slide cycles
-      easing : 'swing',                       // custom easing function
 
       theme : 'basic',                        // basic, dark, light, or stitch
       rounded : false,                        // square or rounded corners
@@ -136,6 +180,7 @@
 
       slides
         .removeClass('slide')
+        .attr('style', '')
         .children()
         .attr('style', '');
     };
@@ -176,6 +221,9 @@
       // cache slide length
       slide.l = slides.length;
 
+      // add slide class to each slide
+      slides.addClass('slide');
+
       // set tab size on first tab elem to calculate px value
       tabs.eq(0).height(settings.tabSize);
       tab.h = tabs.eq(0).height();
@@ -195,9 +243,6 @@
           elem.height(slide.l * tab.h + padding);
         }
       }
-
-      // add slide class to each slide
-      slides.addClass('slide');
     };
 
     setup.getSlideCss = function(index, selected) {
@@ -320,29 +365,46 @@
 
       // bind click and mouseover events
       if (settings.activateOn === 'click') {
-        tabs.on('click.accordionPro touchstart.accordionPro', core.animate);
+        tabs.on('click.accordionPro touchstart.accordionPro', core.animationCycle);
         if (settings.startClosed) tabs.on('click.accordionPro.closed touchstart.accordionPro.closed', setup.startClosed);
       } else if (settings.activateOn === 'mouseover') {
-        tabs.on('click.accordionPro touchstart.accordionPro mouseover.accordionPro', core.animate);
+        tabs.on('click.accordionPro touchstart.accordionPro mouseover.accordionPro', core.animationCycle);
         if (settings.startClosed) tabs.on('click.accordionPro.closed touchstart.accordionPro.closed mouseover.accordionPro.closed', setup.startClosed);
       }
 
-      // swipe events
-      slides.swipe({
-        left : function() {
-          if (orientation) settings.rtl ? methods.prev() : methods.next();
-        },
-        right : function() {
-          if (orientation) settings.rtl ? methods.next() : methods.prev();
-        },
-        up : function() {
-          if (!orientation) methods.next();
-        },
-        down : function() {
-          if (!orientation) methods.prev();
-        },
-        threshold: { x: 100, y: 100 }
-      });
+      // bind touch events (swipe)
+      if (Modernizr.touch) {
+        slides.swipe({
+          left : function() {
+            if (orientation) {
+              if (settings.rtl) {
+                // don't select previous slide if current slide is index zero
+                if (core.currentSlide) methods.prev();
+              } else {
+                methods.next();
+              }
+            }
+          },
+          right : function() {
+            if (orientation) {
+              if (settings.rtl) {
+                methods.next();
+              } else {
+                if (core.currentSlide) methods.prev();
+              }
+            }
+          },
+          up : function() {
+            if (!orientation) methods.next();
+          },
+          down : function() {
+            if (!orientation && core.currentSlide) methods.prev();
+          },
+          threshold: { x: 100, y: 100 }
+        });
+      }
+
+
 
 /*
       // bind hashchange event
@@ -438,8 +500,120 @@
     // animation active flag
     core.animationFlag = false;
 
+    // animate with css transitions, else fallback to jQuery animation
+    core.transition = function(trigger) {
+      var _this = this;
+
+      this
+        .slide
+        .stop(true)
+        .animate(
+          this.position,
+          settings.slideSpeed,
+          function() {
+            // flag ensures that fn is only called one time per triggerSlide
+            if (!core.animationFlag) {
+              // trigger callback in context of sibling div (jQuery wrapped)
+              settings.onSlideClose.call(trigger ? trigger.next.children('div') : _this.prev.next().children('div'));
+              core.animationFlag = true;
+            }
+          });
+    };
+
+    // calculate position of individual slide
+    core.getSlidePosition = function(index, pos) {
+      var position = {};
+
+      if (typeof pos === 'number') {
+        if (orientation) { // horizontal
+          if (settings.rtl) {
+            position = { right : pos + index * tab.h };
+          } else {
+            position = { left : pos + index * tab.h };
+          }
+        } else { // vertical
+          position = { top : pos + index * tab.h };
+        }
+      } else {
+        if (orientation) { // horizontal
+          if (settings.rtl) {
+            position = { right : slide.w + index * tab.h };
+          } else {
+            position = { left : slide.w + index * tab.h };
+          }
+        } else { // vertical
+          position = { top : slide.h + index * tab.h };
+        }
+      }
+
+      return position;
+    };
+
+    // animate single slide
+    core.animateSlide = function(trigger) {
+      var position;
+
+      // set position for single selected tab
+      if (typeof this.position === 'undefined') {
+        this.position = core.getSlidePosition(this.index);
+      } else if (typeof this.position === 'number') {
+        position = this.position;
+        this.position = core.getSlidePosition(this.index, position);
+      }
+
+      // remove, then add selected class
+      slides.removeClass('selected').filter(this.slide).addClass('selected');
+
+      // if slide index not zero
+      if (!!this.index) {
+        core.transition.call(this, trigger);
+
+        // remove, then add selected class
+        slides.removeClass('selected').filter(this.prev).addClass('selected');
+      }
+    };
+
+    // animate group of slides
+    core.animateGroup = function(trigger) {
+      var group = ['left', 'right'];
+
+      $.each(group, function(index, side) {
+        var filterExpr, position;
+
+        if (!index)  {
+          // left side
+          filterExpr = ':lt(' + (trigger.index + 1) + ')';
+          position = 0;
+        } else {
+          // right side
+          filterExpr = ':gt(' + trigger.index + ')';
+          position = orientation ? slide.w : slide.h;
+        }
+
+        slides
+          .filter(filterExpr)
+          .each(function() {
+            var $this = $(this),
+                active = {
+                  tab : $this.children('h2'),
+                  slide : $this,
+                  next : $this.next(),
+                  prev : $this.prev(),
+                  index : slides.index($this),
+                  position : position
+                };
+
+            // trigger item anim, pass original trigger context for callback fn
+            core.animateSlide.call(active, trigger);
+          });
+      });
+
+      // remove, then add selected class
+      slides.removeClass('selected').filter(trigger.slide).addClass('selected');
+    };
+
     // trigger animation cycle
-    core.animate = function() {
+    core.animationCycle = function() {
       var $this = $(this),
           active = {
             tab : $this,
@@ -496,139 +670,7 @@
 */
     };
 
-    core.getSlidePosition = function(index, pos) {
-      var position = {};
 
-      if (typeof pos === 'number') {
-        if (orientation) { // horizontal
-          if (settings.rtl) {
-            position = { right : pos + index * tab.h };
-          } else {
-            position = { left : pos + index * tab.h };
-          }
-        } else { // vertical
-          position = { top : pos + index * tab.h };
-        }
-
-
-
-/*
-        switch (pos) {
-          case 'top':
-            position = { top : index * tab.h };
-            break;
-          case 'bottom':
-            position = { top : slide.h + index * tab.h };
-            break;
-          case 'right':
-            if (settings.rtl) {
-              position = { right : slide.w + index * tab.h };
-            } else {
-              position = { left : slide.w + index * tab.h };
-            }
-            break;
-          case 'left':
-          default:
-            if (settings.rtl) {
-              position = { right : index * tab.h };
-            } else {
-              position = { left : index * tab.h };
-            }
-            break;
-        }
-*/
-      } else {
-        if (orientation) { // horizontal
-          if (settings.rtl) {
-            position = { right : slide.w + index * tab.h };
-          } else {
-            position = { left : slide.w + index * tab.h };
-          }
-        } else { // vertical
-          position = { top : slide.h + index * tab.h };
-        }
-      }
-
-      return position;
-    };
-
-    // animate single slide
-    core.animateSlide = function(trigger) {
-      var _this = this, position;
-
-      // set position for single selected tab
-      if (typeof this.position === 'undefined') {
-        this.position = core.getSlidePosition(this.index);
-      } else if (typeof this.position === 'number') {
-        position = this.position;
-        this.position = core.getSlidePosition(this.index, position);
-      }
-
-      // remove, then add selected class
-      slides.removeClass('selected').filter(this.slide).addClass('selected');
-
-      // if slide index not zero
-      if (!!this.index) {
-        this
-          .slide
-          .stop(true)
-          .animate(
-            this.position,
-            settings.slideSpeed,
-            settings.easing,
-            function() {
-              // flag ensures that fn is only called one time per triggerSlide
-              if (!core.animationFlag) {
-                // trigger callback in context of sibling div (jQuery wrapped)
-                settings.onSlideClose.call(trigger ? trigger.next.children('div') : _this.prev.next().children('div'));
-                core.animationFlag = true;
-              }
-            });
-
-        // remove, then add selected class
-        slides.removeClass('selected').filter(_this.prev).addClass('selected');
-      }
-    };
-
-    // animate group of slides
-    core.animateGroup = function(trigger) {
-      var group = ['left', 'right'];
-
-      $.each(group, function(index, side) {
-        var filterExpr, position;
-
-        if (!index)  {
-          // left side
-          filterExpr = ':lt(' + (trigger.index + 1) + ')';
-          position = 0;
-        } else {
-          // right side
-          filterExpr = ':gt(' + trigger.index + ')';
-          position = orientation ? slide.w : slide.h;
-        }
-
-        slides
-          .filter(filterExpr)
-          .each(function() {
-            var $this = $(this),
-                active = {
-                  tab : $this.children('h2'),
-                  slide : $this,
-                  next : $this.next(),
-                  prev : $this.prev(),
-                  index : slides.index($this),
-                  position : position
-                };
-                // active.position = core.getSlidePosition(active.index, group[index]);
-
-            // trigger item anim, pass original trigger context for callback fn
-            core.animateSlide.call(active, trigger);
-          });
-      });
-
-      // remove, then add selected class
-      slides.removeClass('selected').filter(trigger.slide).addClass('selected');
-    };
 
     core.init = function() {
       setup.ie();
